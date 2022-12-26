c41728ac52312fce8933d5cb80e01a06cc80f930
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2022-12-26 22:30:28 +0100

    Add project files.

---
+++ SecureAPI2/Program.cs
@@ -0,0 +1,40 @@
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Audience = "AAD:ResourceId";
+        options.Authority = $"{"AAD:Instance"}{"AAD:TenantId"}";
+    });
+
+//    options.Audience = "https://localhost:5001/";
+//    options.Authority = "https://localhost:5000/";
+//});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
